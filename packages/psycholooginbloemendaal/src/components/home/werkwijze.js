6f33b9e30b45d5f01d2a6c1db5254761156ccdc6
Ties Hoenderdos <ties@van-ons.nl>
2021-11-21 01:12:13 +0100

    added werkwijze-tarief to homepage

--- packages/psycholooginbloemendaal/src/components/home/werkwijze.js
+++ packages/psycholooginbloemendaal/src/components/home/werkwijze.js
@@ -6,7 +6,15 @@ const WerkwijzePrev = () => {
 			<div className="werkwijze-inner-wrapper">
 				<h2>HOE GA IK TE WERK?</h2>
 				<h4>WERKWIJZE EN TARIEVEN</h4>
-				<div className="werkwijze-picture-wrapper"></div>
+				<div className="werkwijze-picture-wrapper">
+					<img src="https://github.com/DesignerTies/frontity-goed/blob/4c8c570ddde904474264f545ef8ab94c77580026/packages/psycholooginbloemendaal/src/components/assets/foto-werkplek-1.png?raw=true" alt="" />
+					<img src="https://github.com/DesignerTies/frontity-goed/blob/4c8c570ddde904474264f545ef8ab94c77580026/packages/psycholooginbloemendaal/src/components/assets/foto-werkplek-2.png?raw=true" alt="" />
+					<img src="https://raw.githubusercontent.com/DesignerTies/frontity-goed/4c8c570ddde904474264f545ef8ab94c77580026/packages/psycholooginbloemendaal/src/components/assets/foto-werkplek-buiten.png" alt="" />
+				</div>
+				<div className="werkwijze-tarieven-text-wrapper">
+					<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusaminvidunt ut labore et dolore magna </p>
+				</div>
+				<a href="">Lees meer</a>
 			</div>
 		</div>
 	)
